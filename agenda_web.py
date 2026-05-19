2ba662ac415e54f922d75d63db1f923289f09331
Héctor Lombardo <hector.lombardo64@gmail.com>
2026-05-19 19:44:36 -0300

    19:44 19/05/2026

--- agenda_web.py
+++ agenda_web.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 class ValidationError(Exception):
